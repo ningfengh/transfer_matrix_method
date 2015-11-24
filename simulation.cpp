00f0ec16ad3971b597b563a7c18b2f27073fb8ea
Ningfeng Huang <ningfengh@gmail.com>
2015-11-23 16:14:54 -0800

    slightly modified simulation.cpp to make it clearer

--- simulation.cpp
+++ simulation.cpp
@@ -51,58 +51,73 @@ simulation::simulation(string filename)
 void simulation::get_ref_trans(string filename)
 {
 	if (layer_data.size()<1) {cout<<"please add more than one layer"<<endl;return;}
-	complex<double> theta0 (aoi*M_PI/180.0,0.0);
-	complex<double> n0 (1.0,0.0);
-	complex<double> M[2][2];
+	complex	<double> theta0 (aoi*M_PI/180.0,0.0);
+	complex	<double> n0 (1.0,0.0);
+	complex	<double> M[2][2];
+	complex	<double> tmp[2][2];
 	complex <double> EYE (0,1);
+	complex <double> n1, n2;
+	complex <double> theta1, theta2;
+	complex <double> rs, ts;
+	complex <double> delta;
+	complex	<double> ref, trans;
+	
 	int nlayer = layer_data.size();
 	ofstream myfile (filename.c_str());
 	
 	for (int wav_idx=0;wav_idx<npoint;wav_idx++)
 	{
-		complex <double> n1(material_data[layer_data[0]->idx]->get_n(wav_vector[wav_idx]),material_data[layer_data[0]->idx]->get_k(wav_vector[wav_idx]));
-		complex <double> n2;
-		complex <double> theta1 = asin(n0*sin(theta0)/n1);
-		complex <double> theta2;
-		complex <double> rs = (n0*cos(theta0)-n1*cos(theta1))/(n0*cos(theta0)+n1*cos(theta1));
-		complex <double> ts = (2.0*n0*cos(theta0))/(n0*cos(theta0)+n1*cos(theta1));
+		/* calculate the first layer matrix */
+		n1 = complex<double>(material_data[layer_data[0]->idx]->get_n(wav_vector[wav_idx]),material_data[layer_data[0]->idx]->get_k(wav_vector[wav_idx]));
+		theta1 = asin(n0*sin(theta0)/n1);
+		rs = (n0*cos(theta0)-n1*cos(theta1))/(n0*cos(theta0)+n1*cos(theta1));
+		ts = (2.0*n0*cos(theta0))/(n0*cos(theta0)+n1*cos(theta1));
 		M[0][0] = 1.0/ts; M[0][1] = rs/ts;
-		M[1][0] = rs/ts;  M[1][1] = 1.0/ts;    // first layer
+		M[1][0] = rs/ts;  M[1][1] = 1.0/ts;    
 		
+		
+		/* calculate the middle layers */
 		for (int layer_idx = 0;layer_idx<nlayer-1;layer_idx++)
 		{
 			n1 = complex <double>(material_data[layer_data[layer_idx]->idx]->get_n(wav_vector[wav_idx]),material_data[layer_data[layer_idx]->idx]->get_k(wav_vector[wav_idx]));
 			n2 = complex <double>(material_data[layer_data[layer_idx+1]->idx]->get_n(wav_vector[wav_idx]),material_data[layer_data[layer_idx+1]->idx]->get_k(wav_vector[wav_idx]));
 			theta1 = asin(n0*sin(theta0)/n1);
 			theta2 = asin(n0*sin(theta0)/n2);
-			complex <double> delta = 2.0*M_PI/n0*n1*cos(theta1)/wav_vector[wav_idx]*layer_data[layer_idx]->thickness;		
+			delta = 2.0*M_PI/n0*n1*cos(theta1)/wav_vector[wav_idx]*layer_data[layer_idx]->thickness;		
 			rs = (n1*cos(theta1)-n2*cos(theta2))/(n1*cos(theta1)+n2*cos(theta2));
 			ts = (2.0*n1*cos(theta1))/(n1*cos(theta1)+n2*cos(theta2));
-			complex<double> tmp[2][2];
+			
 			tmp[0][0] = M[0][0]*exp(-EYE*delta)/ts+M[0][1]*exp(EYE*delta)*rs/ts;
 			tmp[0][1] = M[0][0]*exp(-EYE*delta)*rs/ts+M[0][1]*exp(EYE*delta)/ts;
 			tmp[1][0] = M[1][0]*exp(-EYE*delta)/ts+M[1][1]*exp(EYE*delta)*rs/ts;
-			tmp[1][1] = M[1][0]*exp(-EYE*delta)*rs/ts+M[1][1]*exp(EYE*delta)/ts;	
+			tmp[1][1] = M[1][0]*exp(-EYE*delta)*rs/ts+M[1][1]*exp(EYE*delta)/ts;
+			
 			M[0][0] = tmp[0][0];
 			M[0][1] = tmp[0][1];
 			M[1][0] = tmp[1][0];
 			M[1][1] = tmp[1][1];
 				
 		}
+		
+		/* calculate last layer */
+		
 		n1 = complex <double>(material_data[layer_data[nlayer-1]->idx]->get_n(wav_vector[wav_idx]),material_data[layer_data[nlayer-1]->idx]->get_k(wav_vector[wav_idx]));
 		theta1 = asin(n0*sin(theta0)/n1);
 		complex <double> delta = 2.0*M_PI/n0*n1*cos(theta1)/wav_vector[wav_idx]*layer_data[nlayer-1]->thickness;		
 		rs = (n1*cos(theta1)-n0*cos(theta0))/(n1*cos(theta1)+n0*cos(theta0));
 		ts = (2.0*n1*cos(theta1))/(n1*cos(theta1)+n0*cos(theta0));
-		complex<double> tmp[2][2];
+		
 		tmp[0][0] = M[0][0]*exp(-EYE*delta)/ts+M[0][1]*exp(EYE*delta)*rs/ts;
 		tmp[0][1] = M[0][0]*exp(-EYE*delta)*rs/ts+M[0][1]*exp(EYE*delta)/ts;
 		tmp[1][0] = M[1][0]*exp(-EYE*delta)/ts+M[1][1]*exp(EYE*delta)*rs/ts;
 		tmp[1][1] = M[1][0]*exp(-EYE*delta)*rs/ts+M[1][1]*exp(EYE*delta)/ts;
-		complex<double> ref = tmp[1][0]/tmp[0][0];
-		complex<double> trans = 1.0/tmp[0][0];
+		
+		ref = tmp[1][0]/tmp[0][0];
+		trans = 1.0/tmp[0][0];
+		
 		ref_vector.push_back(norm(ref));
 		trans_vector.push_back(norm(trans));
+		
 		myfile<<wav_vector[wav_idx]<<"\t"<<norm(ref)<<"\t"<<norm(trans)<<endl;
 	}
 	myfile.close();
